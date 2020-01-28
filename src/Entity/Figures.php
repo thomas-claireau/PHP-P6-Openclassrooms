5d823bcd0cbf894063171156b19a4cb46f0b3531
Thomas Claireau <thomas.claireau@gmail.com>
2020-01-28 18:59:53 +0100

    updtae entity figures

--- src/Entity/Figures.php
+++ src/Entity/Figures.php
@@ -27,6 +27,21 @@ class Figures
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\Column(type="string", length=400)
+     */
+    private $short_description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -58,6 +73,42 @@ class Figures
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->short_description;
+    }
+
+    public function setShortDescription(string $short_description): self
+    {
+        $this->short_description = $short_description;
 
         return $this;
     }

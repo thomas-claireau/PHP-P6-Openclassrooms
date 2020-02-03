39d86dc64daa1472f39c7cd7d8cf678fd3910017
Thomas Claireau <tclaireau@kookline.net>
2020-02-03 08:28:23 +0100

    render register form + system OK

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,11 +1,39 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController {
+
+	/**
+     * @var UserRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+	private $em;
+	
+	/**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserRepository $repository, ObjectManager $em, UserPasswordEncoderInterface $encoder)
+    {
+        $this->repository = $repository;
+		$this->em = $em;
+		$this->encoder = $encoder;
+    }
 
     /**
      * @Route("/login", name="login")
@@ -18,6 +46,28 @@ class SecurityController extends AbstractController {
             'error' => $error,
             'current_menu' => 'login'
         ]);
-    }
+	}
+	
+	/**
+	 * @Route("/register", name="register")
+	 */
+	public function register(Request $request) {
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$user->setPassword($this->encoder->encodePassword($user, $user->getPassword()));
+			$this->em->persist($user);
+			$this->em->flush();
+			$this->addFlash('success', 'Votre inscription a été acceptée. Vous avez reçu un mail de confirmation à l\'adresse que vous nous avez indiqué.');
+			return $this->redirectToRoute('home');
+		}
+
+		return $this->render('security/register.html.twig', [
+			'current_menu' => 'register',
+			'form' => $form->createView(),
+		]);
+	}
 
 }

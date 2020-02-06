d7a9a48c29b2aed0a223afb8e8c5ec5208948015
Thomas Claireau <tclaireau@kookline.net>
2020-02-06 07:34:38 +0100

    admin categorie + style OK

--- src/Controller/Admin/AdminCategoryController.php
+++ src/Controller/Admin/AdminCategoryController.php
@@ -15,70 +15,76 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdminCategoryController extends AbstractController
 {
-    /**
-     * @Route("/", name="admin.category.index", methods={"GET"})
-     */
-    public function index(CategoryRepository $categoryRepository): Response
-    {
-        return $this->render('admin/category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
-        ]);
-    }
+	/**
+	 * @Route("/", name="admin.category.index", methods={"GET"})
+	 */
+	public function index(CategoryRepository $categoryRepository): Response
+	{
+		return $this->render('admin/category/index.html.twig', [
+			'categories' => $categoryRepository->findAll(),
+			'current_menu' => "admin.category.index"
+		]);
+	}
 
-    /**
-     * @Route("/new", name="admin.category.new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
+	/**
+	 * @Route("/new", name="admin.category.new", methods={"GET","POST"})
+	 */
+	public function new(Request $request): Response
+	{
+		$category = new Category();
+		$form = $this->createForm(CategoryType::class, $category);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($category);
-            $entityManager->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($category);
+			$entityManager->flush();
 
-            return $this->redirectToRoute('admin.category.index');
-        }
+			$this->addFlash('success', 'La catégorie a bien été créée');
+			return $this->redirectToRoute('admin.category.index');
+		}
 
-        return $this->render('admin/category/new.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
+		return $this->render('admin/category/new.html.twig', [
+			'category' => $category,
+			'form' => $form->createView(),
+			'current_menu' => "admin.category.new"
+		]);
+	}
 
-    /**
-     * @Route("/{id}/edit", name="admin.category.edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Category $category): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
+	/**
+	 * @Route("/{id}/edit", name="admin.category.edit", methods={"GET","POST"})
+	 */
+	public function edit(Request $request, Category $category): Response
+	{
+		$form = $this->createForm(CategoryType::class, $category);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			$this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('admin.category.index');
-        }
+			$this->addFlash('success', 'La catégorie a bien été modifiée');
+			return $this->redirectToRoute('admin.category.index');
+		}
 
-        return $this->render('admin/category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
+		return $this->render('admin/category/edit.html.twig', [
+			'category' => $category,
+			'form' => $form->createView(),
+			'current_menu' => "admin.category.edit"
+		]);
+	}
 
-    /**
-     * @Route("/{id}", name="admin.category.delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Category $category): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
+	/**
+	 * @Route("/{id}", name="admin.category.delete", methods={"DELETE"})
+	 */
+	public function delete(Request $request, Category $category): Response
+	{
+		if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->remove($category);
+			$entityManager->flush();
+			$this->addFlash('success', 'La catégorie a bien été supprimée');
+		}
 
-        return $this->redirectToRoute('admin.category.index');
-    }
+		return $this->redirectToRoute('admin.category.index');
+	}
 }

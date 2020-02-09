5c04d0ab2c487b98a08df8a9a7de9ca30948b5ee
Thomas Claireau <thomas.claireau@gmail.com>
2020-02-09 18:19:40 +0100

    refacto edit and new page figure

--- assets/js/figure/figure.js
+++ assets/js/figure/figure.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	const figureShowPage = document.querySelector('body.figure-show');
+	const figureEditPage = document.querySelector('body.admin-figure-edit');
 
 	if (figureShowPage) {
 		const iframes = document.querySelectorAll('iframe');
@@ -13,6 +14,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
 					});
 				});
 			});
+		}
+	}
+
+	if (figureEditPage) {
+		const formRemoveFigure = figureEditPage.querySelector('#removeFigure');
+		const containerToMove = figureEditPage.querySelector('.actions-form');
+
+		if (formRemoveFigure && containerToMove) {
+			containerToMove.appendChild(formRemoveFigure);
 		}
 	}
 });

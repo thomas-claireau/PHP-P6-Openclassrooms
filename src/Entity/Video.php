12b6c388bc7239d2e4b639621f4414c0de544fa6
Thomas Claireau <thomas.claireau@gmail.com>
2020-02-11 19:17:10 +0100

    add regex sur iframe video

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -9,49 +10,50 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Video
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $url;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 * @Assert\Regex("/<iframe ?.*>(.*)<\/iframe>/")
+	 */
+	private $url;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Figures", inversedBy="videos")
-     */
-    private $figure;
+	/**
+	 * @ORM\ManyToOne(targetEntity="App\Entity\Figures", inversedBy="videos")
+	 */
+	private $figure;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getUrl(): ?string
-    {
-        return $this->url;
-    }
+	public function getUrl(): ?string
+	{
+		return $this->url;
+	}
 
-    public function setUrl(string $url): self
-    {
-        $this->url = $url;
+	public function setUrl(string $url): self
+	{
+		$this->url = $url;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getFigure(): ?Figures
-    {
-        return $this->figure;
-    }
+	public function getFigure(): ?Figures
+	{
+		return $this->figure;
+	}
 
-    public function setFigure(?Figures $figure): self
-    {
-        $this->figure = $figure;
+	public function setFigure(?Figures $figure): self
+	{
+		$this->figure = $figure;
 
-        return $this;
-    }
+		return $this;
+	}
 }
